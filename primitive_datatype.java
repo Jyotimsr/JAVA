ec42d42427732691abc3bacfad14426cb1bd2bff
Jyotimsr <122531533+Jyotimsr@users.noreply.github.com>
2024-05-17 13:02:34 +0530

    Add files via upload

---
+++ primitive_datatype.java
@@ -0,0 +1,47 @@
+class primitive_datatype {
+    public static void main(String args[]) {
+        System.out.print("jyoti");
+        System.out.println("jyoti");
+        System.out.println("jyoti");
+
+        // PRIMITIVE DATATYPE 
+        //1. INTEGER
+
+        int p=8;
+        int d=9;
+        System.out.println(p+d);
+
+        byte y = 67;
+        System.out.println(y);
+       
+
+        short z = 3759;
+        System.out.println(z);
+
+        long x1 = 948737656;
+        System.out.println(x1);
+        
+        // 2.FLOAT 
+
+        float x = 9;
+        System.out.println(x);
+        float x2 = 7.7f;
+        System.out.println(x2);
+
+        double x3 = 79.098765467f;
+        System.out.println(x3);
+        
+
+        // 3.CHARACTER
+
+        char a1 = '9';
+        System.out.println(a1);
+
+        // BOOLEAN
+
+        boolean b2 = false;
+        System.out.println(b2);
+        boolean c3 = true;
+        System.out.println(c3);
+
+    }}
